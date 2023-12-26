2f60b4e3fd3bf1229868329ef4034a3cc75a4237
abbas-zain00 <abbaszainulabidin00@gmail.com>
2023-12-26 05:14:35 +0000

    added basic code for google sign in and removed form for normal sign in

---
+++ main.js
@@ -0,0 +1,14 @@
+function onSignIn(googleUser) {
+    // Get user profile information
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    // Here, you would handle the sign-in process, like sending the token to your server
+}
